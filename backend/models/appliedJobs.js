bafd52f53973e1d930e9e2f7c89c42577086ee88
Sanskar Sahu <sanskarsahu1704@gmail.com>
2024-12-03 00:07:57 +0530

    Added ability to apply on multiple jobs

--- backend/models/appliedJobs.js
+++ backend/models/appliedJobs.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const appliedJobs = new mongoose.Schema({
-  
-  email: { type: String, required: true ,unique: true}, // Applicant's email
+  email: { type: String, required: true }, // Applicant's email
   resumePath: { type: String, required: true }, // Path to the uploaded resume
   uploadedAt: { type: Date, default: Date.now }, // Timestamp
   requirements: {type: String, required: true}, // Job requirements
   company:{type:String,required: true}, //company
   jobPosition: {type: String,required:true}, // job position
 });
-
+appliedJobs.index({ email: 1, company: 1, jobPosition: 1 }, { unique: true });
 module.exports = mongoose.model('appliedJobs_nexusAI', appliedJobs);
