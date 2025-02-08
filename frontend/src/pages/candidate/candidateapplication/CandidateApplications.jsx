1bce68b3efdccd447bf401d69dcca0df1663d9d1
vivekmishra9343 <vivekmishra9343@gmail.com>
2025-02-09 00:10:57 +0530

    trying to fix vercel error #2
    nothing major

--- frontend/src/pages/candidate/candidateapplication/CandidateApplications.jsx
+++ frontend/src/pages/candidate/candidateapplication/CandidateApplications.jsx
@@ -4,41 +4,40 @@ import { Toaster } from "sonner";
 import ApplicationCard from "../../../components/candidatePage/dashboardapplications/ApplicationCard";
 import StageProgress from "../../../components/candidatePage/dashboardapplications/StageProgress";
 import Sidebar from "../../../components/candidatePage/Sidebar";
-import { mockApplications, mockUser } from "../../../data/mockData";
-import { Avatar } from "../../../components/ui/Avatar";
+
 import { useDashboardData } from "../../../hooks/useDashBoardData";
 
 const CandidateApplications = () => {
   const { applications, error, loading } = useDashboardData();
   if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error}</div>; // agar user job fill nhi kiya ho 
+  // if (error) return <div>Error: {error}</div>; // agar user job fill nhi kiya ho
   // If job not applied at all
-  if(error){
+  if (error) {
     return (
       <div className='bg-slate-900'>
-      <Toaster position='top-right' />
-      <Sidebar />
+        <Toaster position='top-right' />
+        <Sidebar />
 
-      <div className='ml-64'>
-        <div className='max-w-7xl mx-auto px-4 py-8'>
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            className='flex items-center gap-4 mb-8'
-          ></motion.div>
+        <div className='ml-64'>
+          <div className='max-w-7xl mx-auto px-4 py-8'>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              className='flex items-center gap-4 mb-8'
+            ></motion.div>
 
-          <div className='grid gap-6 '>
-          <h1 className='text-center text-white text-xl'>No jobs applied yet!</h1>
+            <div className='grid gap-6 '>
+              <h1 className='text-center text-white text-xl'>
+                No jobs applied yet!
+              </h1>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    )
-  };
-
+    );
+  }
 
   // If jobs applied
-
 
   console.log(applications);
   return (
